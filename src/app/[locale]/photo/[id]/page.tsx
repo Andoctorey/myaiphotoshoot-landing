e89a1661bbf36107ea012eee3ab372595eaf972c
andrey <a.d.yegorov@gmail.com>
2025-05-18 02:17:03 +0700

    Refactor PhotoPage to handle params with proper typing and await support

--- src/app/[locale]/photo/[id]/page.tsx
+++ src/app/[locale]/photo/[id]/page.tsx
@@ -1,3 +1,14 @@
 import { default as PhotoPageComponent } from '@/app/photo/[id]/page';
 
-export default PhotoPageComponent; 
+interface PhotoPageProps {
+  params: {
+    id: string;
+    locale: string;
+  }
+}
+
+export default async function PhotoPage({ params }: PhotoPageProps) {
+  // With Next.js 15, params may be a Promise that needs to be awaited
+  // We need to create a properly typed object to pass to the component
+  return <PhotoPageComponent params={{ id: params.id }} />;
+} 
